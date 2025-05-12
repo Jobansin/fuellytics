00a395c7e83a64f555e9a724570af7a73668d3a8
Jobansin <jobanpreetsngh10@gmail.com>
2025-05-12 18:15:58 -0400

    initial commit

---
+++ src/main/java/com/fuellytics/FuellyticsApplication.java
@@ -0,0 +1,13 @@
+package com.fuellytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuellyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuellyticsApplication.class, args);
+	}
+
+}
